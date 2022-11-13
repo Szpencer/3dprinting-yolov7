3645b82f71540ddb42ce9273bc63d394d7a4841d
Szpencer <103958913+Szpencer@users.noreply.github.com>
2022-11-13 09:52:52 +0100

    Update detect.py
    Signed-off-by: Szpencer <103958913+Szpencer@users.noreply.github.com>

--- detect.py
+++ detect.py
@@ -123,7 +123,10 @@ def detect(save_img=False):
                         line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
                         with open(txt_path + '.txt', 'a') as f:
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
-
+                        print("-------------------------------------------------------------------------")
+                        print("ikipszilonvéhá értéke:")
+                        print(xywh)
+                        print("-------------------------------------------------------------------------")
                     if save_img or view_img:  # Add bbox to image
                         label = f'{names[int(cls)]} {conf:.2f}'
                         plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
